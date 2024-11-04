8b5cbe5e0c988520a0f6b6f1594234ba72c7ea5a
dg20 <dg20@ford.com>
2024-11-05 01:15:44 +0530

    delete part updated

--- E-library-ui/src/app/books/component/book-details/book-details.component.ts
+++ E-library-ui/src/app/books/component/book-details/book-details.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../../service/books.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { response } from 'express';
+import {DialogModule} from "primeng/dialog";
+import {ReactiveFormsModule} from "@angular/forms";
+import {Button} from "primeng/button";
+import {CommonModule} from "@angular/common";
+
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, DialogModule, ReactiveFormsModule, Button, CommonModule],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.css'
 })
 export class BookDetailsComponent implements OnInit {
   bookId: any;
   book: any;
+  displayDialog: boolean = false;
 
   constructor(private bookService: BooksService,private route:ActivatedRoute){
     this.bookId = this.route.snapshot.paramMap.get('id');
@@ -30,5 +35,13 @@ export class BookDetailsComponent implements OnInit {
       console.log(response);
       alert("Book Deleted Successfully");
     })
+  }
+
+  hideDialog() {
+    this.displayDialog = false;
+  }
+
+  showDialog() {
+    this.displayDialog = true;
   }
 }
